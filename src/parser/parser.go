8b6e94b1500e4faf2784bb825ea6606319a57b4f
Alexandre Normand <alexandre.normand@gmail.com>
2013-03-19 21:49:11 -0700

    Initial commit of dexcom parser service.

---
+++ src/parser/parser.go
@@ -0,0 +1,52 @@
+package parser
+
+import (
+	"encoding/xml"
+	"io"
+	"os"
+	"bufio"
+)
+
+type Meter struct {
+	InternalTime string `xml:"InternalTime,attr"`
+	DisplayTime  string `xml:"DisplayTime,attr"`
+	Value        int    `xml:"Value,attr"`
+}
+
+func Parse(filepath string, writer io.Writer, handler func(*Meter, io.Writer)) {
+	// open input file
+	fi, err := os.Open(filepath)
+	if err != nil { panic(err) }
+	// close fi on exit and check for its returned error
+	defer func() {
+		if fi.Close() != nil {
+			panic(err)
+		}
+	}()
+	// make a read buffer
+	r := bufio.NewReader(fi)
+
+	decoder := xml.NewDecoder(r)
+
+	for {
+		// Read tokens from the XML document in a stream.
+		t, _ := decoder.Token()
+		if t == nil {
+			break
+		}
+		// Inspect the type of the token just read.
+		switch se := t.(type) {
+		case xml.StartElement:
+			// If we just read a StartElement token
+			// ...and its name is "page"
+			if se.Name.Local == "Meter" {
+				var read Meter
+				// decode a whole chunk of following XML into the
+				// variable p which is a Page (se above)
+				decoder.DecodeElement(&read, &se)
+				// Do some stuff with the page.
+				handler(&read, writer)
+			}
+		}
+	}
+}
