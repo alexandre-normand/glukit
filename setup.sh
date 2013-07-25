9d25df90ad47ab45539aa4702036fbe681711596
Alexandre Normand <alexandre.normand@gmail.com>
2013-07-24 23:18:46 -0700

    Add symlinks to setup script as I realized this was missing when I had to recreate the repo.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-git clone git@github.com:grd/stat.git lib/github.com/grd/stat
+if [ ! -d "lib/github.com/grd/stat" ]; then
+	git clone git@github.com:grd/stat.git lib/github.com/grd/stat
+fi
+
+mkdir -p code.google.com/p
+cd code.google.com/p
+ln -fs ../../../goauth2
+mkdir -p google-api-go-client
+cd google-api-go-client
+ln -fs ../../../../google-api-go-client/googleapi
