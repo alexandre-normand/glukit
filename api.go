aca52232bfad656585c762d9d9fa7332e82be978
Alexandre Normand <alexandre.normand@gmail.com>
2014-03-14 22:12:53 -0700

    Add Skeleton For Buffered Writing of Data Received via REST Api.

--- api.go
+++ api.go
@@ -7,6 +7,15 @@ import (
 // processNewCalibrationData Handles a Post to the calibration endpoint and
 // handles all data to be stored for a given user
 func processNewCalibrationData(writer http.ResponseWriter, request *http.Request) {
+
+	writer.WriteHeader(200)
+
+	// TODO: Parse the calibration reads from the json
+
+	// TODO: Write values to the buffer
+
+	// TODO: Flush to trigger the persistence
+
 	// context := appengine.NewContext(request)
 	// user := user.Current(context)
 }
