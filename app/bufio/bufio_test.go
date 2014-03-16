83dba26205ea233b6b99e20bd1b5948caee3b2dc
Alexandre Normand <alexandre.normand@gmail.com>
2014-03-15 22:17:41 -0700

    Add First Unit Test for Buffered Calibration Read Writer.

--- app/bufio/bufio_test.go
+++ app/bufio/bufio_test.go
@@ -1,5 +1,40 @@
 package bufio_test
 
 import (
-	. "bufio"
+	. "github.com/alexandre-normand/glukit/app/bufio"
+	"github.com/alexandre-normand/glukit/app/model"
+	"log"
+	"testing"
 )
+
+type statsCalibrationWriter struct {
+	total      int
+	batchCount int
+}
+
+func (w *statsCalibrationWriter) WriteCalibrations(p []model.CalibrationRead) (n int, err error) {
+	log.Printf("Got some calibrations")
+	w.total += len(p)
+	w.batchCount++
+
+	return len(p), nil
+}
+
+func TestBufferSimple(t *testing.T) {
+	statsWriter := new(statsCalibrationWriter)
+	w := NewWriterSize(statsWriter, 10)
+	calibrations := make([]model.CalibrationRead, 10)
+	for i := 0; i < 10; i++ {
+		calibrations[i] = model.CalibrationRead{model.Timestamp{"", 0}, 75}
+	}
+	w.WriteCalibrations(calibrations)
+	w.Flush()
+
+	if statsWriter.total != 10 {
+		t.Errorf("simple batch test failed: got a total of %d but expected %d", statsWriter.total, 10)
+	}
+
+	if statsWriter.batchCount != 1 {
+		t.Errorf("simple batch test failed: got a total of %d but expected %d", statsWriter.batchCount, 1)
+	}
+}
