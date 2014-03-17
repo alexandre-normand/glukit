47d3ee4d140348e56d006b58b19853d80988312c
Alexandre Normand <alexandre.normand@gmail.com>
2014-03-16 21:17:58 -0700

    Add Missing Glue To Get Calibration Reads Persisted to the Data Store.
    * Still needs the buffering to split on day boundaries like what's done
      in parser.go now.
    * Need to find better names for the bufio and io packages.

--- app/io/io.go
+++ app/io/io.go
@@ -1,8 +1,11 @@
 package io
 
 import (
+	"appengine"
+	"appengine/datastore"
 	"errors"
 	"github.com/alexandre-normand/glukit/app/model"
+	"github.com/alexandre-normand/glukit/app/store"
 )
 
 // ErrShortWrite means that a write accepted fewer bytes than requested
@@ -17,4 +20,26 @@ var ErrShortWrite = errors.New("short write")
 // Write must return a non-nil error if it returns n < len(p).
 type CalibrationWriter interface {
 	WriteCalibrations(p []model.CalibrationRead) (n int, err error)
+}
+
+type DataStoreCalibrationWriter struct {
+	c appengine.Context
+	k *datastore.Key
+}
+
+func NewDataStoreCalibrationWriter(context appengine.Context, userProfileKey *datastore.Key) *DataStoreCalibrationWriter {
+	w := new(DataStoreCalibrationWriter)
+	w.c = context
+	w.k = userProfileKey
+	return w
+}
+
+func (w *DataStoreCalibrationWriter) WriteCalibrations(p []model.CalibrationRead) (n int, err error) {
+	dayOfCalibrationReads := make([]model.DayOfCalibrationReads, 1)
+	dayOfCalibrationReads[0] = model.DayOfCalibrationReads{p}
+	if keys, err := store.StoreCalibrationReads(w.c, w.k, dayOfCalibrationReads); err != nil {
+		return 0, err
+	} else {
+		return len(keys), nil
+	}
 }
