933f2fc35edaf2c16072ba66b0ad10da8d678038
Alexandre Normand <alexandre.normand@gmail.com>
2014-04-09 20:45:44 -0700

    Add Partial Implementation of Internal Oauth Structs Storage.

--- app/store/osinstorage.go
+++ app/store/osinstorage.go
@@ -5,19 +5,43 @@ import (
 	"appengine/datastore"
 	"errors"
 	"github.com/RangelReale/osin"
+	"time"
 )
 
 type OsinAppEngineStore struct {
 	c appengine.Context
 }
 
-type oclient struct {
+type oClient struct {
 	Id          string `datastore:"Id"`
 	Secret      string `datastore:"Secret,noindex"`
 	RedirectUri string `datastore:"RedirectUri,noindex"`
 }
 
-// TODO: create structs
+// Authorization data
+type oAuthorizeData struct {
+	Client      *oClient  `datastore:"-,noindex"`
+	Code        string    `datastore:"Code"`
+	ExpiresIn   int32     `datastore:"ExpiresIn"`
+	Scope       string    `datastore:"Scope"`
+	RedirectUri string    `datastore:"RedirectUri"`
+	State       string    `datastore:"State"`
+	CreatedAt   time.Time `datastore:"CreatedAt"`
+}
+
+// AccessData
+type oAccessData struct {
+	Client        *oClient        `datastore:"-,noindex"`
+	AuthorizeData *oAuthorizeData `datastore:"-,noindex"`
+	AccessData    *oAccessData    `datastore:"-,noindex"`
+	AccessToken   string          `datastore:"accessToken"`
+	RefreshToken  string          `datastore:"refreshToken"`
+	ExpiresIn     int32           `datastore:"expiresIn,noindex"`
+	Scope         string          `datastore:"scope,noindex"`
+	RedirectUri   string          `datastore:"redirectUri,noindex"`
+	CreatedAt     time.Time       `datastore:"createdAt,noindex"`
+}
+
 func NewOsinAppEngineStore(c appengine.Context) *OsinAppEngineStore {
 	r := &OsinAppEngineStore{c}
 
@@ -36,8 +60,7 @@ func NewOsinAppEngineStore(c appengine.Context) *OsinAppEngineStore {
 func (s *OsinAppEngineStore) addClient(c *osin.Client) error {
 	s.c.Debugf("AddClient: %s\n", c.Id)
 	key := datastore.NewKey(s.c, "osin.client", c.Id, 0, nil)
-	oclient := oclient{c.Id, c.Secret, c.RedirectUri}
-	_, err := datastore.Put(s.c, key, &oclient)
+	_, err := datastore.Put(s.c, key, newInternalClient(c))
 
 	if err != nil {
 		return err
@@ -46,24 +69,64 @@ func (s *OsinAppEngineStore) addClient(c *osin.Client) error {
 	return nil
 }
 
+func newInternalClient(c *osin.Client) *oClient {
+	if c == nil {
+		return nil
+	}
+	return &oClient{c.Id, c.Secret, c.RedirectUri}
+}
+
+func newOsinClient(c *oClient) *osin.Client {
+	if c == nil {
+		return nil
+	}
+	return &osin.Client{c.Id, c.Secret, c.RedirectUri, nil}
+}
+
+func (d *oAuthorizeData) Load(c <-chan datastore.Property) error {
+	if err := datastore.LoadStruct(d, c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *oAuthorizeData) Save(c chan<- datastore.Property) error {
+	return datastore.SaveStruct(d, c)
+}
+
 func (s *OsinAppEngineStore) GetClient(id string) (*osin.Client, error) {
 	s.c.Debugf("GetClient: %s\n", id)
 	key := datastore.NewKey(s.c, "osin.client", id, 0, nil)
-	client := new(oclient)
+	client := new(oClient)
 	err := datastore.Get(s.c, key, client)
 
 	if err != nil {
 		s.c.Warningf("Error looking up client by id [%s]: [%v]", id, err)
 		return nil, errors.New("Client not found")
 	}
-	osinClient := osin.Client{client.Id, client.Secret, client.RedirectUri, nil}
-	return &osinClient, nil
+	osinClient := newOsinClient(client)
+	return osinClient, nil
+}
+
+func newInternalAuthorizeData(d *osin.AuthorizeData) *oAuthorizeData {
+	if d == nil {
+		return nil
+	}
+	return &oAuthorizeData{newInternalClient(d.Client), d.Code, d.ExpiresIn, d.Scope, d.RedirectUri, d.State, d.CreatedAt}
+}
+
+func newOsinAuthorizeData(d *oAuthorizeData) *osin.AuthorizeData {
+	if d == nil {
+		return nil
+	}
+	return &osin.AuthorizeData{newOsinClient(d.Client), d.Code, d.ExpiresIn, d.Scope, d.RedirectUri, d.State, d.CreatedAt, nil}
 }
 
 func (s *OsinAppEngineStore) SaveAuthorize(data *osin.AuthorizeData) error {
 	s.c.Debugf("SaveAuthorize: %s\n", data.Code)
 	key := datastore.NewKey(s.c, "authorize.data", data.Code, 0, nil)
-	_, err := datastore.Put(s.c, key, data)
+	_, err := datastore.Put(s.c, key, newInternalAuthorizeData(data))
 	if err != nil {
 		return err
 	}
@@ -74,13 +137,13 @@ func (s *OsinAppEngineStore) SaveAuthorize(data *osin.AuthorizeData) error {
 func (s *OsinAppEngineStore) LoadAuthorize(code string) (*osin.AuthorizeData, error) {
 	s.c.Debugf("LoadAuthorize: %s\n", code)
 	key := datastore.NewKey(s.c, "authorize.data", code, 0, nil)
-	authorizeData := new(osin.AuthorizeData)
+	authorizeData := new(oAuthorizeData)
 	err := datastore.Get(s.c, key, authorizeData)
 	if err != nil {
 		s.c.Infof("Authorization data not found for code [%s]: %v", code, err)
 		return nil, errors.New("Authorize not found")
 	}
-	return authorizeData, nil
+	return newOsinAuthorizeData(authorizeData), nil
 }
 
 func (s *OsinAppEngineStore) RemoveAuthorize(code string) error {
@@ -94,17 +157,32 @@ func (s *OsinAppEngineStore) RemoveAuthorize(code string) error {
 	return nil
 }
 
+func newInternalAccessData(d *osin.AccessData) *oAccessData {
+	if d == nil {
+		return nil
+	}
+	return &oAccessData{newInternalClient(d.Client), newInternalAuthorizeData(d.AuthorizeData), newInternalAccessData(d.AccessData), d.AccessToken, d.RefreshToken, d.ExpiresIn, d.Scope, d.RedirectUri, d.CreatedAt}
+}
+
+func newOsinAccessData(d *oAccessData) *osin.AccessData {
+	if d == nil {
+		return nil
+	}
+	return &osin.AccessData{newOsinClient(d.Client), newOsinAuthorizeData(d.AuthorizeData), newOsinAccessData(d.AccessData), d.AccessToken, d.RefreshToken, d.ExpiresIn, d.Scope, d.RedirectUri, d.CreatedAt, nil}
+}
+
 func (s *OsinAppEngineStore) SaveAccess(data *osin.AccessData) error {
 	s.c.Debugf("SaveAccess: %s\n", data.AccessToken)
 	key := datastore.NewKey(s.c, "access.data", data.AccessToken, 0, nil)
-	_, err := datastore.Put(s.c, key, data)
+	internalAccessData := newInternalAccessData(data)
+	_, err := datastore.Put(s.c, key, internalAccessData)
 	if err != nil {
 		return err
 	}
 
 	if data.RefreshToken != "" {
 		key = datastore.NewKey(s.c, "access.refresh", data.RefreshToken, 0, nil)
-		_, err := datastore.Put(s.c, key, data)
+		_, err := datastore.Put(s.c, key, internalAccessData)
 		if err != nil {
 			return err
 		}
@@ -115,14 +193,14 @@ func (s *OsinAppEngineStore) SaveAccess(data *osin.AccessData) error {
 func (s *OsinAppEngineStore) LoadAccess(code string) (*osin.AccessData, error) {
 	s.c.Debugf("LoadAccess: %s\n", code)
 	key := datastore.NewKey(s.c, "access.data", code, 0, nil)
-	accessData := new(osin.AccessData)
+	accessData := new(oAccessData)
 	err := datastore.Get(s.c, key, accessData)
 	if err != nil {
 		s.c.Infof("Access data not found for code [%s]: %v", code, err)
 		return nil, errors.New("Access data not found")
 	}
 
-	return accessData, nil
+	return newOsinAccessData(accessData), nil
 }
 
 func (s *OsinAppEngineStore) RemoveAccess(code string) error {
@@ -139,13 +217,13 @@ func (s *OsinAppEngineStore) RemoveAccess(code string) error {
 func (s *OsinAppEngineStore) LoadRefresh(code string) (*osin.AccessData, error) {
 	s.c.Debugf("LoadRefresh: %s\n", code)
 	key := datastore.NewKey(s.c, "access.refresh", code, 0, nil)
-	accessData := new(osin.AccessData)
+	accessData := new(oAccessData)
 	_, err := datastore.Put(s.c, key, accessData)
 	if err != nil {
 		s.c.Infof("Refresh data not found for code [%s]: %v", code, err)
 		errors.New("Refresh not found")
 	}
-	return accessData, nil
+	return newOsinAccessData(accessData), nil
 }
 
 func (s *OsinAppEngineStore) RemoveRefresh(code string) error {
--- app/store/osinstorage_test.go
+++ app/store/osinstorage_test.go
@@ -2,8 +2,10 @@ package store_test
 
 import (
 	"appengine/aetest"
+	"github.com/RangelReale/osin"
 	. "github.com/alexandre-normand/glukit/app/store"
 	"testing"
+	"time"
 )
 
 func TestGetClient(t *testing.T) {
@@ -16,6 +18,27 @@ func TestGetClient(t *testing.T) {
 	osinStorage := NewOsinAppEngineStore(c)
 	_, err = osinStorage.GetClient("834681386231.mygluk.it")
 
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAccessDataStorage(t *testing.T) {
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	osinStorage := NewOsinAppEngineStore(c)
+	client, err := osinStorage.GetClient("834681386231.mygluk.it")
+	d := osin.AccessData{client, nil, nil, "token", "test", 0, "scope", "uri", time.Now(), nil}
+	err = osinStorage.SaveAccess(&d)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = osinStorage.LoadAccess("token")
 	if err != nil {
 		t.Fatal(err)
 	}
