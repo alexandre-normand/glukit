25f6f35bb5d2255a91ceb5ef1b3e2eb761dcfb2c
Alexandre Normand <alexandre.normand@gmail.com>
2014-04-13 14:46:24 -0700

    Implement Solution to Osin Storage Testing.
    * Made Signatures with Context and Delegate Calls with Request to the
      WithContext version.
    * Tests WithContext methods with a test appengine.Context.

--- app/store/osinstorage_test.go
+++ app/store/osinstorage_test.go
@@ -4,7 +4,6 @@ import (
 	"appengine/aetest"
 	. "github.com/alexandre-normand/glukit/app/store"
 	"github.com/alexandre-normand/osin"
-	"net/http"
 	"testing"
 	"time"
 )
@@ -15,9 +14,10 @@ func TestGetClient(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer c.Close()
+	t.Logf("Request is [%v] from context [%v]", c.Request(), c)
 
-	osinStorage := NewOsinAppEngineStore(c.Request().(*http.Request))
-	_, err = osinStorage.GetClient("834681386231.mygluk.it", c.Request().(*http.Request))
+	osinStorage := NewOsinAppEngineStoreWithContext(c)
+	_, err = osinStorage.GetClientWithContext("834681386231.mygluk.it", c)
 
 	if err != nil {
 		t.Fatal(err)
@@ -30,16 +30,17 @@ func TestAccessDataStorage(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer c.Close()
+	t.Logf("Request is [%v] from context [%v]", c.Request(), c)
 
-	osinStorage := NewOsinAppEngineStore(c.Request().(*http.Request))
-	client, err := osinStorage.GetClient("834681386231.mygluk.it", c.Request().(*http.Request))
-	d := osin.AccessData{client, nil, nil, "token", "test", 0, "scope", "uri", time.Now(), nil}
-	err = osinStorage.SaveAccess(&d, c.Request().(*http.Request))
+	osinStorage := NewOsinAppEngineStoreWithContext(c)
+	client, err := osinStorage.GetClientWithContext("834681386231.mygluk.it", c)
+	d := osin.AccessData{client, nil, nil, "token", "test", 0, "scope", "uri", time.Now(), TEST_USER}
+	err = osinStorage.SaveAccessWithContext(&d, c)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	_, err = osinStorage.LoadAccess("token", c.Request().(*http.Request))
+	_, err = osinStorage.LoadAccessWithContext("token", c)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -52,15 +53,15 @@ func TestAuthorizeDataStorage(t *testing.T) {
 	}
 	defer c.Close()
 
-	osinStorage := NewOsinAppEngineStore(nil)
-	client, err := osinStorage.GetClient("834681386231.mygluk.it", c.Request().(*http.Request))
-	d := osin.AuthorizeData{client, "code", 0, "scope", "uri", "state", time.Now(), nil}
-	err = osinStorage.SaveAuthorize(&d, c.Request().(*http.Request))
+	osinStorage := NewOsinAppEngineStoreWithContext(c)
+	client, err := osinStorage.GetClientWithContext("834681386231.mygluk.it", c)
+	d := osin.AuthorizeData{client, "code", 0, "scope", "uri", "state", time.Now(), TEST_USER}
+	err = osinStorage.SaveAuthorizeWithContext(&d, c)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	_, err = osinStorage.LoadAuthorize("code", c.Request().(*http.Request))
+	_, err = osinStorage.LoadAuthorizeWithContext("code", c)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -73,27 +74,27 @@ func TestFullAccessDataStorage(t *testing.T) {
 	}
 	defer c.Close()
 
-	osinStorage := NewOsinAppEngineStore(c.Request().(*http.Request))
-	client, err := osinStorage.GetClient("834681386231.mygluk.it", c.Request().(*http.Request))
-	d := osin.AuthorizeData{client, "code", 0, "scope", "uri", "state", time.Now(), nil}
-	err = osinStorage.SaveAuthorize(&d, c.Request().(*http.Request))
+	osinStorage := NewOsinAppEngineStoreWithContext(c)
+	client, err := osinStorage.GetClientWithContext("834681386231.mygluk.it", c)
+	d := osin.AuthorizeData{client, "code", 0, "scope", "uri", "state", time.Now(), TEST_USER}
+	err = osinStorage.SaveAuthorizeWithContext(&d, c)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	previousAccess := osin.AccessData{client, nil, nil, "token", "test", 0, "scope", "uri", time.Now(), nil}
-	err = osinStorage.SaveAccess(&previousAccess, c.Request().(*http.Request))
+	previousAccess := osin.AccessData{client, nil, nil, "token", "test", 0, "scope", "uri", time.Now(), TEST_USER}
+	err = osinStorage.SaveAccessWithContext(&previousAccess, c)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	access := osin.AccessData{client, &d, &previousAccess, "token", "test", 0, "scope", "uri", time.Now(), nil}
-	err = osinStorage.SaveAccess(&access, c.Request().(*http.Request))
+	access := osin.AccessData{client, &d, &previousAccess, "token", "test", 0, "scope", "uri", time.Now(), TEST_USER}
+	err = osinStorage.SaveAccessWithContext(&access, c)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	_, err = osinStorage.LoadAccess("token", c.Request().(*http.Request))
+	_, err = osinStorage.LoadAccessWithContext("token", c)
 	if err != nil {
 		t.Fatal(err)
 	}
