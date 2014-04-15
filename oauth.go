918165dfc2fd618cec22ec6d14d4ee6eee27e1a1
Alexandre Normand <alexandre.normand@gmail.com>
2014-04-14 21:09:41 -0700

    Save Email Address in Oauth UserData.

--- oauth.go
+++ oauth.go
@@ -2,6 +2,7 @@ package glukit
 
 import (
 	"appengine"
+	"appengine/user"
 	"github.com/alexandre-normand/glukit/app/store"
 	"github.com/alexandre-normand/osin"
 	"net/http"
@@ -15,12 +16,14 @@ func initOauthProvider(writer http.ResponseWriter, request *http.Request) {
 	server := osin.NewServer(sconfig, store.NewOsinAppEngineStoreWithRequest(request))
 	r.HandleFunc("/authorize", func(w http.ResponseWriter, req *http.Request) {
 		c := appengine.NewContext(req)
+		user := user.Current(c)
 		resp := server.NewResponse()
 		c.Debugf("Processing authorization request: %v", r)
 		if ar := server.HandleAuthorizeRequest(resp, req); ar != nil {
 			// Nothing to do since the page is already login restricted by gae app configuration
 
 			ar.Authorized = true
+			ar.UserData = user.Email
 			server.FinishAuthorizeRequest(resp, req, ar)
 		}
 		if resp.IsError && resp.InternalError != nil {
@@ -32,10 +35,12 @@ func initOauthProvider(writer http.ResponseWriter, request *http.Request) {
 	// Access token endpoint
 	r.HandleFunc("/token", func(w http.ResponseWriter, req *http.Request) {
 		c := appengine.NewContext(req)
+		user := user.Current(c)
 		resp := server.NewResponse()
 		c.Debugf("Processing token request: %v", req)
 		if ar := server.HandleAccessRequest(resp, req); ar != nil {
 			ar.Authorized = true
+			ar.UserData = user.Email
 			server.FinishAccessRequest(resp, req, ar)
 		}
 		osin.OutputJSON(resp, w, req)
