574e83f5eeb1451a72e5cd3403d1e9746f7e26ea
Alexandre Normand <alexandre.normand@gmail.com>
2014-05-07 21:00:38 -0700

    Add Test to Show Calibration/Carb Buffered IO Problem with Lost Data.

--- app/streaming/carbstreamer_test.go
+++ app/streaming/carbstreamer_test.go
@@ -1,6 +1,7 @@
 package streaming_test
 
 import (
+	"github.com/alexandre-normand/glukit/app/bufio"
 	"github.com/alexandre-normand/glukit/app/model"
 	. "github.com/alexandre-normand/glukit/app/streaming"
 	"log"
@@ -12,21 +13,29 @@ type statsCarbWriter struct {
 	total      int
 	batchCount int
 	writeCount int
+	batches    map[int64][]model.Carb
+}
+
+func NewStatsCarbsWriter() *statsCarbWriter {
+	w := new(statsCarbWriter)
+	w.batches = make(map[int64][]model.Carb)
+
+	return w
 }
 
 func (w *statsCarbWriter) WriteCarbBatch(p []model.Carb) (n int, err error) {
 	log.Printf("WriteCarbBatch with [%d] elements: %v", len(p), p)
-	dayOfCarbs := make([]model.DayOfCarbs, 1)
-	dayOfCarbs[0] = model.DayOfCarbs{p}
 
-	return w.WriteCarbBatches(dayOfCarbs)
+	return w.WriteCarbBatches([]model.DayOfCarbs{model.DayOfCarbs{p}})
 }
 
 func (w *statsCarbWriter) WriteCarbBatches(p []model.DayOfCarbs) (n int, err error) {
 	log.Printf("WriteCarbBatch with [%d] batches: %v", len(p), p)
 	for _, dayOfData := range p {
 		w.total += len(dayOfData.Carbs)
+		w.batches[dayOfData.Carbs[0].EpochTime] = dayOfData.Carbs
 	}
+
 	log.Printf("WriteCarbBatch with total of %d", w.total)
 	w.batchCount += len(p)
 	w.writeCount++
@@ -39,7 +48,7 @@ func (w *statsCarbWriter) Flush() error {
 }
 
 func TestWriteOfDayCarbBatch(t *testing.T) {
-	statsWriter := new(statsCarbWriter)
+	statsWriter := NewStatsCarbsWriter()
 	w := NewCarbStreamerDuration(statsWriter, time.Hour*24)
 	carb := make([]model.Carb, 25)
 
@@ -65,7 +74,7 @@ func TestWriteOfDayCarbBatch(t *testing.T) {
 }
 
 func TestWriteOfHourlyCarbBatch(t *testing.T) {
-	statsWriter := new(statsCarbWriter)
+	statsWriter := NewStatsCarbsWriter()
 	w := NewCarbStreamerDuration(statsWriter, time.Hour*1)
 	carb := make([]model.Carb, 13)
 
@@ -106,7 +115,7 @@ func TestWriteOfHourlyCarbBatch(t *testing.T) {
 }
 
 func TestWriteOfMultipleCarbBatches(t *testing.T) {
-	statsWriter := new(statsCarbWriter)
+	statsWriter := NewStatsCarbsWriter()
 	w := NewCarbStreamerDuration(statsWriter, time.Hour*1)
 	carb := make([]model.Carb, 25)
 
@@ -143,5 +152,43 @@ func TestWriteOfMultipleCarbBatches(t *testing.T) {
 
 	if statsWriter.writeCount != 3 {
 		t.Errorf("TestWriteOfMultipleCarbBatches failed: got a writeCount of %d but expected %d", statsWriter.writeCount, 3)
+	}
+}
+
+func TestCarbStreamerWithBufferedIO(t *testing.T) {
+	statsWriter := NewStatsCarbsWriter()
+	bufferedWriter := bufio.NewCarbWriterSize(statsWriter, 2)
+	w := NewCarbStreamerDuration(bufferedWriter, time.Hour*24)
+
+	ct, _ := time.Parse("02/01/2006 00:15", "18/04/2014 00:00")
+
+	for b := 0; b < 3; b++ {
+		for i := 0; i < 48; i++ {
+			readTime := ct.Add(time.Duration(b*48+i) * 30 * time.Minute)
+			w.WriteCarb(model.Carb{model.Timestamp{"", readTime.Unix()}, float32(b*48 + i), model.UNDEFINED_READ})
+		}
+	}
+
+	w.Close()
+
+	firstBatchTime, _ := time.Parse("02/01/2006 00:15", "18/04/2014 00:00")
+	if value, ok := statsWriter.batches[firstBatchTime.Unix()]; !ok {
+		t.Errorf("TestCarbStreamerWithBufferedIO test failed: count not find a batch starting with a read time of [%v] in batches: [%v]", firstBatchTime.Unix(), statsWriter.batches)
+	} else {
+		t.Logf("Value is [%s]", value)
+	}
+
+	secondBatchTime := firstBatchTime.Add(time.Duration(24) * time.Hour)
+	if value, ok := statsWriter.batches[secondBatchTime.Unix()]; !ok {
+		t.Errorf("TestCarbStreamerWithBufferedIO test failed: count not find a batch starting with a read time of [%v] in batches: [%v]", secondBatchTime.Unix(), statsWriter.batches)
+	} else {
+		t.Logf("Value is [%s]", value)
+	}
+
+	thirdBatchTime := firstBatchTime.Add(time.Duration(48) * time.Hour)
+	if value, ok := statsWriter.batches[thirdBatchTime.Unix()]; !ok {
+		t.Errorf("TestCarbStreamerWithBufferedIO test failed: count not find a batch starting with a read time of [%v] in batches: [%v]", thirdBatchTime.Unix(), statsWriter.batches)
+	} else {
+		t.Logf("Value is [%s]", value)
 	}
 }
