2bfbf0aa40825681bda2537a44a24b945a314295
Alexandre Normand <alexandre.normand@gmail.com>
2014-07-05 21:10:18 -0700

    Implement Reconciliation of Injections.

--- app/store/endtoendstorage_test.go
+++ app/store/endtoendstorage_test.go
@@ -142,3 +142,70 @@ func TestEndToEndMergeOfCalibrationBatches(t *testing.T) {
 		t.Errorf("Expected [29] reads but got [%d]", len(reads))
 	}
 }
+
+func TestEndToEndMergeOfInjectionBatches(t *testing.T) {
+	c, key := setup(t)
+	defer c.Close()
+
+	w := store.NewDataStoreInjectionBatchWriter(c, key)
+	bufferedWriter := bufio.NewInjectionWriterSize(w, 5)
+	s := streaming.NewInjectionStreamerDuration(bufferedWriter, apimodel.DAY_OF_DATA_DURATION)
+
+	// Write first chunk
+	r := make([]apimodel.Injection, 25)
+	firstChunkStart, _ := time.Parse("02/01/2006 15:04", "18/04/2015 01:00")
+	for i := 0; i < 25; i++ {
+		readTime := firstChunkStart.Add(time.Duration(i) * time.Hour)
+		r[i] = apimodel.Injection{apimodel.Time{apimodel.GetTimeMillis(readTime), "America/Montreal"}, float32(i), "Humalog", "Bolus"}
+	}
+	s, _ = s.WriteInjections(r)
+	s, _ = s.Flush()
+	s, _ = s.Close()
+
+	lowerBound, _ := time.Parse("02/01/2006 15:04", "18/04/2015 00:00")
+	upperBound, _ := time.Parse("02/01/2006 15:04", "20/04/2015 02:00")
+	reads, err := store.GetInjections(c, TEST_USER, lowerBound, upperBound)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reads == nil {
+		t.Errorf("Reads is nil. Expected retrieval with results")
+	}
+
+	if reads[0].GetTime().Unix() != firstChunkStart.Unix() {
+		t.Errorf("First reads of batch doesn't match expected time. Expected [%v], got [%v]", firstChunkStart, reads[0].GetTime())
+	}
+
+	if len(reads) != 25 {
+		t.Errorf("Expected [25] injections but got [%d]: %v", len(reads), reads)
+	}
+
+	// Simulate a second batch of writes that overlaps partly with the second batch and confirm that the total
+	// number of reads matches the expected (i.e. no duplicates)
+	secondChunkStart, _ := time.Parse("02/01/2006 15:04", "18/04/2015 05:00")
+	r = make([]apimodel.Injection, 25)
+	for i := 0; i < 25; i++ {
+		readTime := secondChunkStart.Add(time.Duration(i) * time.Hour)
+		r[i] = apimodel.Injection{apimodel.Time{apimodel.GetTimeMillis(readTime), "America/Montreal"}, float32(i), "Humalog", "Bolus"}
+	}
+	s, _ = s.WriteInjections(r)
+	s, _ = s.Flush()
+	s, _ = s.Close()
+
+	lowerBound, _ = time.Parse("02/01/2006 15:04", "18/04/2015 00:00")
+	upperBound, _ = time.Parse("02/01/2006 15:04", "20/04/2015 02:00")
+	reads, err = store.GetInjections(c, TEST_USER, lowerBound, upperBound)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reads[0].GetTime().Unix() != firstChunkStart.Unix() {
+		t.Errorf("First injection of batch doesn't match expected time. Expected [%v], got [%v]", secondChunkStart, reads[0].GetTime())
+	}
+
+	if len(reads) != 29 {
+		t.Errorf("Expected [29] injections but got [%d]", len(reads))
+	}
+}
