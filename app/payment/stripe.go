4973e916f4a73293215a0012a0f131e9f05c036b
Alexandre Normand <alexandre.normand@gmail.com>
2014-07-22 21:27:28 -0700

    Implement Stripe Charge.

--- app/payment/stripe.go
+++ app/payment/stripe.go
@@ -4,9 +4,17 @@ package payment
 import (
 	"appengine"
 	"appengine/urlfetch"
+	"appengine/user"
+	"fmt"
 	"github.com/alexandre-normand/glukit/app/config"
 	"github.com/cosn/stripe"
 	"net/http"
+	"strconv"
+)
+
+const (
+	STRIPE_TOKEN    = "stripeToken"
+	DONATION_AMOUNT = "donation-amount"
 )
 
 type StripeClient struct {
@@ -19,10 +27,43 @@ func NewStripeClient(appConfig *config.AppConfig) (c *StripeClient) {
 	return c
 }
 
-func (c *StripeClient) SubmitDonation(r *http.Request) {
+func (c *StripeClient) SubmitDonation(r *http.Request) error {
 	context := appengine.NewContext(r)
+	user := user.Current(context)
 	client := urlfetch.Client(context)
 
-	stripe := &stripe.Client{}
-	stripe.Init(c.apiKey, client, nil)
+	sc := &stripe.Client{}
+	sc.Init(c.apiKey, client, nil)
+
+	r.ParseForm()
+	token := r.FormValue(STRIPE_TOKEN)
+	amountInCentsVal := r.FormValue(DONATION_AMOUNT)
+	amountInCents, err := strconv.ParseUint(amountInCentsVal, 10, 32)
+	if err != nil {
+		return err
+	}
+
+	amountInDollars := float32(amountInCents) / 100.
+
+	context.Debugf("Received donation request of [%.2f] with stripe token [%s] for user [%s]", amountInDollars, token, user)
+
+	params := &stripe.ChargeParams{
+		Token:    token,
+		Amount:   amountInCents,
+		Currency: stripe.USD,
+		Desc:     fmt.Sprintf("Generous donation of $%.2f to Glukit.", amountInDollars),
+		Meta: map[string]string{
+			"email": user.Email,
+		},
+		Statement: "Donation",
+		Email:     user.Email,
+	}
+
+	if charge, err := sc.Charges.Create(params); err != nil {
+		return err
+	} else {
+		context.Infof("Charged donation of [%.2f] to [%s] successfully: [%v]", amountInDollars, user.Email, charge)
+	}
+
+	return nil
 }
