c015ea08eaa6de731b943e8b762ec6311f891ceb
Alexandre Normand <alexandre.normand@gmail.com>
2014-12-19 21:58:30 -0800

    Round A1C value to 1 Decimal for Display.

--- view/js/browser.js
+++ view/js/browser.js
@@ -462,7 +462,7 @@ function addTrendToProfile(pathPrefix, dashboardData, sectionName) {
         if (data && data.length > 0) {
             var mostRecentA1C = data[0].Value;
             var angle = getAngleForA1C(mostRecentA1C)
-            document.getElementById("a1c").innerHTML = mostRecentA1C;
+            document.getElementById("a1c").innerHTML = mostRecentA1C.toFixed(1);
             $(".a1cNeedle").attr("style", "-ms-transform: rotate(" + angle + "deg); -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); ");
         }
     });
